ba8b188e2dc8bdd8c9641d8917ab8b5dff50b13c
harsh <harsh.saxena@thoughts2binary.com>
2024-03-20 01:58:21 +0530

    pushing code for dice game

---
+++ main.js
@@ -0,0 +1,33 @@
+const myButton = document.getElementById("myButton");
+
+let yourScore = document.querySelector("#yourScore");
+let compScore = document.querySelector("#compScore");
+console.log(yourScore.innerText);
+console.log(compScore.innerText);
+let winner = null;
+title = document.querySelector("h3");
+
+function playGame() {
+    console.log("clicked");
+    let randNumber1 = Math.ceil(Math.random()*6);
+    document.querySelector(".img1").setAttribute("src", `images/dice${randNumber1}.png`);
+
+    let randNumber2 = Math.ceil(Math.random()*6);
+    document.querySelector(".img2").setAttribute("src", `images/dice${randNumber2}.png`);
+    
+    if (randNumber1 > randNumber2) {
+        title.innerText = "You win!";
+        yourScoreValue = yourScore.innerText;
+        yourScoreValue++;
+        yourScore.innerText = yourScoreValue;
+    } else if (randNumber1 < randNumber2) {
+        title.innerText = "Computer wins!";
+        compScoreValue = compScore.innerText;
+        compScoreValue++
+        compScore.innerText = compScoreValue;
+    } else {
+        title.innerText = "Match drawn!";
+    }
+}
+
+myButton.addEventListener("click", playGame);
